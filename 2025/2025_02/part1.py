f0700c952b6107f14ec699f82b6419e80310a63e
Dorien <dvleeuwen.99@gmail.com>
2025-12-01 09:50:47 +0100

    prepare for day 2

---
+++ 2025/2025_02/part1.py
@@ -0,0 +1,13 @@
+input = []
+
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, 'test1.txt')
+
+with open(file_path, 'r') as file:
+    for line in file:
+        # process each line
+        input.append((line.strip()))
+
+print(input)

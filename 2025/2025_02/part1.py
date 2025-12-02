fc2ee1a5fe356957da0c55707743529d911e5244
Dorien <dvleeuwen.99@gmail.com>
2025-12-02 08:08:12 +0100

    Day 2 solutiona

--- 2025/2025_02/part1.py
+++ 2025/2025_02/part1.py
@@ -3,11 +3,26 @@ input = []
 import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, 'test1.txt')
+file_path = os.path.join(script_dir, 'input.txt')
 
 with open(file_path, 'r') as file:
     for line in file:
         # process each line
-        input.append((line.strip()))
+        input.append((line.strip().split(',')))
 
 print(input)
+res1 = 0
+res2 = 0
+for item in input[0]:
+    first, second = item.split('-')
+    for i in range(int(first), int(second)+1):
+        j = str(i)
+        for k in range(2,len(j)+1):
+            n = int(len(j)/k)
+            if j[0:n] * k == j:
+                if k == 2:
+                    res1 += i
+                res2 += i
+                break
+print('result part 1', res1)
+print('result part 2', res2)
